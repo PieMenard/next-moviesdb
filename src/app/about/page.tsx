b56233eb3da852dbff7e9574bca73675be916ef7
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-15 14:30:40 -0400

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our database website! Here you can find info on your favorite
+        movies, and your not so favorite as well.
+      </p>
+    </div>
+  );
+};
+
+export default About;
