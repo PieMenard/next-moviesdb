eccb7ed9f57816f7300a1c7d1befbdb21237be16
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-16 11:01:52 -0400

    add loading spinner

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div className="flex justify- mt-16">
+      <img className="h-52" src="spinner.svg" alt="loading spinner" />)
+    </div>
+  );
+};
+
+export default loading;
