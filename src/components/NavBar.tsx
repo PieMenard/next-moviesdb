81300c87c045086894dc666699978d9bb165fcb6
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-16 16:17:27 -0400

    add suspense

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import NavBarItem from './NavBarItem';
 
 const NavBar = () => {
   return (
     <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-      <NavBarItem title="Trending" param="fetchTrending" />
-      <NavBarItem title="Top Rated" param="fetchTopRated" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavBarItem title="Trending" param="fetchTrending" />
+        <NavBarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 };
