a668472f2634ebb1bcf667953a156a4308484f54
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-16 12:10:48 -0400

    change order card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,10 +21,10 @@ const Card = ({ movie }: cardProps) => {
           alt="movie poster"
         />
         <div className="p-2">
-          <p className="line-clamp-2 text-md">{movie.overview}</p>
           <h2 className="text-lg font-bold truncate">
             {movie.title || movie.name}
           </h2>
+          <p className="line-clamp-2 text-md">{movie.overview}</p>
           <p className="flex items-center">
             {movie.release_date || movie.first_air_date}
             <FiThumbsUp className="h-5 mr-1 ml-3" />
